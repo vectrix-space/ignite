7cc8cd8e45f3b63ab69badd2fc9a8bb02772fe4b
connorhartley <vectrixu+gh@gmail.com>
2021-02-10 21:38:24 +1300

    Improve example and shade in event

--- example/src/main/java/com/mineteria/example/ExampleMod.java
+++ example/src/main/java/com/mineteria/example/ExampleMod.java
@@ -5,22 +5,26 @@ import com.mineteria.ignite.api.Platform;
 import com.mineteria.ignite.api.config.ModConfig;
 import com.mineteria.ignite.api.event.Subscribe;
 import com.mineteria.ignite.api.event.platform.PlatformInitializeEvent;
+import org.apache.logging.log4j.Logger;
 
 import java.nio.file.Path;
 
 public final class ExampleMod {
+  private final Logger logger;
   private final Platform platform;
   private final Path configuration;
 
   @Inject
-  public ExampleMod(final Platform platform,
+  public ExampleMod(final Logger logger,
+                    final Platform platform,
                     final @ModConfig Path configuration) {
+    this.logger = logger;
     this.platform = platform;
     this.configuration = configuration;
   }
 
   @Subscribe
   public void onInitialize(final PlatformInitializeEvent event) {
-
+    this.logger.info("Hello Initialization!");
   }
 }
