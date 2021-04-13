74531cb6fb5d352a69cad60d8e4feedff1688a64
connorhartley <vectrixu+gh@gmail.com>
2021-04-13 14:26:13 +1200

    release: 0.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "space.vectrix.ignite"
-version = "0.3.0-SNAPSHOT"
+version = "0.3.0"
 description = "Bootstraps the Minecraft Server with ModLauncher to apply Mixins and Access Transformers from mods."
 
 subprojects {
