6af2c9a0cf69f7d2fec605b6b23ba6cd0082e496
connorhartley <vectrixu@gmail.com>
2021-10-13 20:36:18 +1300

    bump shadow plugin to 7.1.0

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -8,6 +8,6 @@ repositories {
 
 dependencies {
   implementation("net.kyori", "indra-common", "2.0.6")
-  implementation("gradle.plugin.com.github.jengelman.gradle.plugins", "shadow", "7.0.0")
+  implementation("gradle.plugin.com.github.jengelman.gradle.plugins", "shadow", "7.1.0")
   implementation("de.marcphilipp.gradle", "nexus-publish-plugin", "0.4.0")
 }
