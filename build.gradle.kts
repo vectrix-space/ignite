1027dcf185267bb5ee86a841bdb287d4267408bc
connorhartley <vectrixu@gmail.com>
2021-10-25 16:41:14 +1300

    snapshot: 0.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
 }
 
 group = "space.vectrix.ignite"
-version = "0.5.3"
+version = "0.6.0-SNAPSHOT"
 description = "Bootstraps the Minecraft Server with ModLauncher to apply Mixins and Access Wideners from mods."
