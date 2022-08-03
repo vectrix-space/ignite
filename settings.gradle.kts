90de7baa5dc2a9a5d378f330291bc6a3082f0dab
connorhartley <vectrixu@gmail.com>
2022-08-03 18:56:05 +1200

    update gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
-pluginManagement {
-  includeBuild("build-logic")
-}
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "ignite-parent"
+
+includeBuild("build-logic")
 
 sequenceOf(
   "api",
